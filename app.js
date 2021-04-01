9c2664dcd06cfc9de0f1de8f769b15f0824b9fbd
ML6503 <ml6503@gmail.com>
2021-04-02 00:20:44 +0300

    refactor: replace id selectors with classes as per BEM rule 2.1 of advanced CSS guide

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementsByClassName("new-task-input")[0];//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementsByClassName("incomplete-tasks")[0];//ul of .incompleteTasks
+var completedTasksHolder=document.getElementsByClassName("completed-tasks")[0];//completed-tasks
 
 
 //New task list item
@@ -64,7 +64,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
   console.log("Add Task...");
-  //Create a new list item with the text from the #new-task:
+  //Create a new list item with the text from the .new-task:
   if (!taskInput.value) return;
   var listItem=createNewTaskElement(taskInput.value);
 
@@ -122,7 +122,7 @@ var deleteTask=function(){
 var taskCompleted=function(){
   console.log("Complete Task...");
 
-  //Append the task list item to the #completed-tasks
+  //Append the task list item to the .completed-tasks
   var listItem=this.parentNode;
   completedTasksHolder.appendChild(listItem);
   var label=listItem.querySelector("label");
